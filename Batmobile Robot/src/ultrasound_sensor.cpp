319e0d73e7fc704390935a9603c65f964e277355
rio-okada <rokada@usf.edu>
2023-02-21 17:56:49 -0500

    empty funciton defined for compilation

--- Batmobile Robot/src/ultrasound_sensor.cpp
+++ Batmobile Robot/src/ultrasound_sensor.cpp
@@ -1,2 +1,19 @@
-
+#include <Arduino.h>
 #include "ultrasound_sensor.h"
+
+Ultrasound::Ultrasound(int triggerPin, int echoPin)
+    : triggerPin(triggerPin), echoPin(echoPin)      // set pins
+{ 
+}
+
+void Ultrasound::begin()
+{
+    pinMode(triggerPin, OUTPUT);    // set trigger pin as output
+    pinMode(echoPin, INPUT);        // set echo pin as input
+}
+
+int Ultrasound::getDistance()
+{
+    // will implement later
+    return 0;
+}
