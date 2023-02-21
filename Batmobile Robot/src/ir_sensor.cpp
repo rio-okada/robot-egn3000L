b0731bc8f5e66a018d844084aa67375ad9f9c3a9
rio-okada <rokada@usf.edu>
2023-02-21 17:56:36 -0500

    empty functions defined for compilation

--- Batmobile Robot/src/ir_sensor.cpp
+++ Batmobile Robot/src/ir_sensor.cpp
@@ -1,2 +1,18 @@
-
+#include <Arduino.h>
 #include "ir_sensor.h"
+
+IR::IR(int pin)
+    : pin(pin)
+{
+}
+
+void IR::begin()
+{
+    pinMode(pin, INPUT);
+}
+
+bool IR::detectObject()
+{
+    // will implement later
+    return true;
+}
--- Batmobile Robot/src/main.cpp
+++ Batmobile Robot/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
+#include "ultrasound_sensor.h"
+#include "ir_sensor.h"
+
+Ultrasound proximity_sensor(9, 8);     // ultrasonic sensor with triggerPin = 9 and echoPin = 8
+IR ir_sensor1(1);   // connect to pin 1
+IR ir_sensor2(4);   // connect to pin 4
+
 void setup() {
-  // put your setup code here, to run once:
+  // begin() functions set pinModes
+  proximity_sensor.begin();
+  ir_sensor1.begin();
+  ir_sensor2.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
