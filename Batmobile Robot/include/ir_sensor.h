35cc5a3a95073e88b2e3605b041ca79f4549b292
rio-okada <rokada@usf.edu>
2023-02-21 17:56:16 -0500

    skelton classes defined

--- Batmobile Robot/include/ir_sensor.h
+++ Batmobile Robot/include/ir_sensor.h
@@ -1,4 +1,15 @@
 #ifndef IR_H
 #define IR_H
 
+class IR {
+    public:
+        IR() = delete;
+        IR(int pin);    // pin = Arduino pin nubmer to connect IR sensor
+        void begin();   // set pin modes
+        bool detectObject();    // returns true if object is detected. false otherwise
+
+    private:
+        int pin;    // arduino pin number that IR sensor is connected to
+};
+
 #endif IR_H
--- Batmobile Robot/include/ultrasound_sensor.h
+++ Batmobile Robot/include/ultrasound_sensor.h
@@ -1,4 +1,15 @@
 #ifndef ULTRASOUND_H
 #define ULTRASOUND_H
 
+class Ultrasound {
+    public:
+        Ultrasound() = delete;
+        Ultrasound(int triggerPin, int echoPin);
+        void begin();       // sets pinMode
+        int getDistance();  // triggers the sensor and returns reading
+    private:
+        int triggerPin;     // pin number for trigger 
+        int echoPin;        // pin number for echo
+};
+
 #endif
