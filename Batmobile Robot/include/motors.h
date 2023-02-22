d02c11428a37b75ef5fd43bef1e9b57c8ee8dc3c
rio-okada <rokada@usf.edu>
2023-02-21 21:53:56 -0500

    declartion and definiton for movement functions

--- Batmobile Robot/include/motors.h
+++ Batmobile Robot/include/motors.h
@@ -2,6 +2,9 @@
 #define MOTORS_H
 
 /* Library for moving the robot */
-
+void moveForward(int seconds);
+void moveBack(int seconds);
+void turnLeft(int seconds);
+void turnRight(int seconds);
 
 #endif 
--- Batmobile Robot/src/motors.cpp
+++ Batmobile Robot/src/motors.cpp
@@ -1,3 +1,22 @@
 #include <Arduino.h>
 #include "motors.h"
 
+void moveForward(int seconds)
+{
+
+}
+
+void moveBack(int seconds)
+{
+
+}
+
+void turnLeft(int seconds)
+{
+
+}
+
+void turnRight(int seconds)
+{
+
+}
